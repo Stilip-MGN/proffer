1bfd705419531e8fbc4383f007ccf2dd6a2e536a
Andrew Krysh <vip.krysh@mail.ru>
2022-10-19 21:29:56 +0500

    изменил на BehaviorSubject

--- app/src/main/java/studio/stilip/proffer/app/fragments/search/SearchViewModel.kt
+++ app/src/main/java/studio/stilip/proffer/app/fragments/search/SearchViewModel.kt
@@ -4,7 +4,7 @@ import androidx.lifecycle.ViewModel
 import dagger.hilt.android.lifecycle.HiltViewModel
 import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers
 import io.reactivex.rxjava3.schedulers.Schedulers
-import io.reactivex.rxjava3.subjects.PublishSubject
+import io.reactivex.rxjava3.subjects.BehaviorSubject
 import studio.stilip.proffer.domain.entities.Ad
 import studio.stilip.proffer.domain.usecase.search.GetRecommendedAdsUseCase
 import javax.inject.Inject
@@ -14,17 +14,14 @@ class SearchViewModel @Inject constructor(
     private val getRecommendedAds: GetRecommendedAdsUseCase
 ) : ViewModel() {
 
-    val ads = PublishSubject.create<List<Ad>>().apply { getRecommended() }
+    val ads = BehaviorSubject.create<List<Ad>>().apply { getRecommended() }
 
-
-    fun getRecommended() {
-
+    private fun getRecommended() {
         getRecommendedAds()
             .subscribeOn(Schedulers.io())
             .observeOn(AndroidSchedulers.mainThread())
             .subscribe { bb ->
                 ads.onNext(bb)
             }
-
     }
 }
