2163950d054cc8c99d062d376ce456b07c767dd3
Andrew Krysh <vip.krysh@mail.ru>
2022-10-20 16:39:39 +0500

    верстка фрагмента избранное

--- app/src/main/java/studio/stilip/proffer/app/fragments/favorites/FavoritesFragment.kt
+++ app/src/main/java/studio/stilip/proffer/app/fragments/favorites/FavoritesFragment.kt
@@ -3,10 +3,13 @@ package studio.stilip.proffer.app.fragments.favorites
 import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
 import studio.stilip.proffer.R
 import studio.stilip.proffer.databinding.FragmentFavoritesBinding
 
 class FavoritesFragment : Fragment(R.layout.fragment_favorites) {
+
+    private val viewModel: FavoritesViewModel by viewModels()
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
