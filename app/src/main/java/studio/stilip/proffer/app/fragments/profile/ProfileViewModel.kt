cf0551f3af85befe6d1ff99914bac323f217321b
Andrew Krysh <vip.krysh@mail.ru>
2022-10-22 16:57:26 +0500

    переименование на profile

--- app/src/main/java/studio/stilip/proffer/app/fragments/profile/ProfileViewModel.kt
+++ app/src/main/java/studio/stilip/proffer/app/fragments/profile/ProfileViewModel.kt
@@ -1,10 +1,10 @@
-package studio.stilip.proffer.app.fragments.notifications
+package studio.stilip.proffer.app.fragments.profile
 
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
-class NotificationsViewModel : ViewModel() {
+class ProfileViewModel : ViewModel() {
 
     private val _text = MutableLiveData<String>().apply {
         value = "This is notifications Fragment"
