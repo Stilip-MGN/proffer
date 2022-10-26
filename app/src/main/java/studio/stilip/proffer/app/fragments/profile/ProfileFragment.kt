4611332f087a12de2eaabe93c2dc911c1e67f03d
Andrew Krysh <vip.krysh@mail.ru>
2022-10-26 20:59:06 +0500

    первоначальная вёрстка профиля

--- app/src/main/java/studio/stilip/proffer/app/fragments/profile/ProfileFragment.kt
+++ app/src/main/java/studio/stilip/proffer/app/fragments/profile/ProfileFragment.kt
@@ -1,42 +1,15 @@
 package studio.stilip.proffer.app.fragments.profile
 
 import android.os.Bundle
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
-import android.widget.TextView
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.ViewModelProvider
+import studio.stilip.proffer.R
 import studio.stilip.proffer.databinding.FragmentProfileBinding
 
-class ProfileFragment : Fragment() {
+class ProfileFragment : Fragment(R.layout.fragment_profile) {
 
-    private var _binding: FragmentProfileBinding? = null
-
-    // This property is only valid between onCreateView and
-    // onDestroyView.
-    private val binding get() = _binding!!
-
-    override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View {
-        val notificationsViewModel =
-            ViewModelProvider(this).get(ProfileViewModel::class.java)
-
-        _binding = FragmentProfileBinding.inflate(inflater, container, false)
-        val root: View = binding.root
-
-        val textView: TextView = binding.textNotifications
-        notificationsViewModel.text.observe(viewLifecycleOwner) {
-            textView.text = it
-        }
-        return root
-    }
-
-    override fun onDestroyView() {
-        super.onDestroyView()
-        _binding = null
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val binding = FragmentProfileBinding.bind(view)
     }
 }
