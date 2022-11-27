2a1eaa022116a94f6219a38c9c4c6f293edc52a7
Andrew Krysh <vip.krysh@mail.ru>
2022-11-27 21:36:49 +0500

    добавил в навигацию fragment_category_spares, а также эффекты нажатия

--- app/src/main/java/studio/stilip/proffer/app/fragments/ads/CategorySparesFragment.kt
+++ app/src/main/java/studio/stilip/proffer/app/fragments/ads/CategorySparesFragment.kt
@@ -4,30 +4,23 @@ import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
-import androidx.navigation.Navigation.findNavController
 import studio.stilip.proffer.R
 import studio.stilip.proffer.app.fragments.HostViewModel
-import studio.stilip.proffer.databinding.FragmentCategorySelectionBinding
+import studio.stilip.proffer.databinding.FragmentCategorySparesBinding
 
-class CategorySelectionFragment : Fragment(R.layout.fragment_category_selection) {
+class CategorySparesFragment : Fragment(R.layout.fragment_category_spares) {
 
     private val hostViewModel: HostViewModel by activityViewModels()
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        val binding = FragmentCategorySelectionBinding.bind(view)
+        val binding = FragmentCategorySparesBinding.bind(view)
 
         hostViewModel.setBottomBarVisible(false)
 
         with(binding) {
             btnBack.setOnClickListener {
                 requireActivity().onBackPressed()
-            }
-
-            categorySpares.setOnClickListener {
-                findNavController(view).navigate(
-                    R.id.action_navigation_category_selection_to_navigation_category_spares
-                )
             }
         }
     }
