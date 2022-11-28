8c3c3d3d2bf7785f5a51c355e8dfabe954bc417e
Andrew Krysh <vip.krysh@mail.ru>
2022-11-28 15:04:35 +0500

    Добавил в новигацию и создал SignInFragment, RegistrationFragment

---
+++ app/src/main/java/studio/stilip/proffer/app/fragments/authorization/RegistrationFragment.kt
@@ -0,0 +1,32 @@
+package studio.stilip.proffer.app.fragments.authorization
+
+import android.os.Bundle
+import android.view.View
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.activityViewModels
+import androidx.navigation.Navigation
+import dagger.hilt.android.AndroidEntryPoint
+import studio.stilip.proffer.R
+import studio.stilip.proffer.app.HostViewModel
+import studio.stilip.proffer.databinding.FragmentRegistrationBinding
+
+@AndroidEntryPoint
+class RegistrationFragment : Fragment(R.layout.fragment_registration) {
+
+    private val hostViewModel: HostViewModel by activityViewModels()
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val binding = FragmentRegistrationBinding.bind(view)
+
+        hostViewModel.setBottomBarVisible(false)
+
+        with(binding) {
+            btnToSignIn.setOnClickListener {
+                Navigation.findNavController(view).navigate(
+                    R.id.action_navigation_registration_to_navigation_sing_in
+                )
+            }
+        }
+    }
+}
